98639394214a4f65f7eeced351f61724af665c11
dvrkdev <dvrk.devx@gmail.com>
2026-03-28 15:26:12 +0500

    fsm context

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,10 +1,18 @@
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
 
 import bot.keyboards as kb
 
 router = Router()
+
+
+class Register(StatesGroup):
+    name = State()
+    number = State()
+    age = State()
 
 
 @router.message(CommandStart())
@@ -64,3 +72,33 @@ async def category(callback: Callback):
     # await callback.answer("You have selected a category.")
     await callback.answer("This is alert message!", show_alert=True)
     await callback.message.answer("Hello!")
+
+
+@router.message(Command("register"))
+async def register(message: Message, state: FSMContext):
+    await state.set_state(Register.name)
+    await message.answer("What is your name?")
+
+
+@router.message(Register.name)
+async def name_handler(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Register.number)
+    await message.answer("Enter your phone number!")
+
+
+@router.message(Register.number)
+async def number_handler(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    await state.set_state(Register.age)
+    await message.answer("How old are you?")
+
+
+@router.message(Register.age)
+async def age_handler(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    await state.clear()
+    await message.answer(
+        f"Thank you!\nName: {data['name']}\nPhone No.: {data['number']}\nAge: {data['age']}"
+    )
